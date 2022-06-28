9ace63f04e89828a5fc3c62379d3f0d83ddf8b26
WadimSh <wadim11@yandex.ru>
2022-06-28 22:26:27 +0300

    fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const rateLimiter = require('./middlewares/rateLimit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
